0b93888049305a70bb05e2f61e4caf5d36713cea
hellocreep <hellocreep@gmail.com>
2013-11-04 17:06:00 +0800

    changed node vertion

--- node/163-music.js
+++ node/163-music.js
@@ -1,5 +1,7 @@
 var http = require('http'),
+	fs = require('fs'),
 	qs = require('querystring'),
+	url = require('url'),
 	conf = require('./conf.json');
 
 var HEADERS = {
@@ -7,15 +9,74 @@ var HEADERS = {
 	'Referer': 'http://music.163.com/'
 }
 
-function downloadSongs() {
+// https://gist.github.com/wilsonpage/1393666
+function urlReq(req_url, options, cb) {
+	if(typeof options === "function"){ cb = options; options = {}; }
 
+	req_url = url.parse(req_url);
+
+	var settings = {
+		host: req_url.hostname,
+		port: req_url.port || 80,
+		path: req_url.path,
+		headers: options.headers || {},
+		method: options.method || 'get'
+	}
+
+	if(options.params) {
+		options.params = qs.stringify(options.params);
+		settings.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+		settings.headers['Content-Length'] = options.params.length;
+	}
+
+	var req = http.request(settings);
+
+	if(options.params){ req.write(options.params) };
+
+	req.on('response', function(res) {
+		res.body = '';
+		res.setEncoding('utf-8');
+
+		res.on('data', function(chunk) {
+			res.body += chunk;
+		});
+
+		res.on('end', function() {
+			cb(res.body, res);
+		});
+	});
+
+	req.end();
 }
 
-function getAlbum() {
+function downloadSongs(songs, folder) {
+	songs.forEach(function(obj, index, list) {
+		var link = obj.mp3Url;
+		var info = {
+			name: obj.name,
+			artist: obj.artists[0].name,
+			album: obj.album.name,
+			track_num: index
+		}
 
+		if(folder) {
+			fs.mkdir(folder);
+		} else {
+			folder = conf.dest;
+		}
+
+		var file = fs.createWriteStream(folder + '/' + info.name+'.mp3');	
+
+		urlReq(link, {
+			method: 'GET',
+			headers: HEADERS
+		}, function(body, res) {
+			res.pipe(file);
+		});
+	});
 }
 
-var commandOption, fileName;
+var commandOption, fileName, args
 
 fileName = process.argv[2];
 
@@ -24,44 +85,76 @@ if(fileName && fileName.indexOf('-') === 0) {
 }
 
 if(commandOption === 's') {
-	var params, options;
+	var params;
+
+	args = process.argv[3];
 
 	params = {
-		'csrf_token': '',
-		'id': '386835',
-		'ids': '[386835]'
+		csrf_token: '',
+		id: args,
+		ids: '['+args+']'
 	}
 
-	options = {
-		hostname: 'music.163.com',
-		port: 80,
-		path: '/api/song/detail/?' + qs.stringify(params),
-		method: 'get',
-		headers: HEADERS
-	};
-
-	var req = http.request(options, function(res) {
-		res.setEncoding('utf8');
-		var data = '';	
-		res.on('data', function(chunk) {
-			data += chunk;
-		});
-		res.on('end', function() {
-			console.log(JSON.parse(data))
-		});
+	urlReq('http://music.163.com/api/song/detail/?'+qs.stringify(params), {
+		headers: HEADERS,
+		method: 'GET'
+	}, function(body, res) {
+		console.log(body)
+		downloadSongs(JSON.parse(body).songs);
 	});
-
-	req.on('error', function(e) {
-		console.log('problem with request: ' + e.message);
-	});
-
-	req.end();
 }
 
 if(commandOption === 'a') {
+	args = process.argv[3];
 
+	urlReq('http://music.163.com/api/album/'+args, {
+		headers: HEADERS,
+		method: 'GET',
+		params: {
+			id: args,
+			csrf_token: ''
+		}
+	}, function(body, res) {
+		var data = JSON.parse(body);
+		if(data['album']) {
+			var album = data['album'];
+			var folder = conf.dest + '/' + album.name;
+			downloadSongs(album.songs, folder);
+		} else {
+			console.log(data);
+		}
+	});
 }
 
 if(commandOption === 'se') {
+	args = process.argv.slice(3).toString().replace(',', ' ');
 
+	urlReq('http://music.163.com/api/search/suggest/web?csrf_token=',{
+		method: 'POST',
+		headers: HEADERS,
+		params: {
+			limit: 20,
+			s: args
+		}
+	}, function(body, res) {
+		var data = JSON.parse(body).result;
+		if(data.albums) {
+			console.log('-------------------------Album----------------------------\n');
+			var albums = data.albums;
+			albums.forEach(function(obj, index) {
+				console.log(obj.name + '-' + obj.artist.name +'\n');
+				console.log(obj.id + '\n');
+			});
+		}
+		if(data.songs) {
+			console.log('-------------------------Song----------------------------\n');
+			var songs = data.songs;
+			songs.forEach(function(obj, index) {
+				console.log(obj.name + '-' + obj.artists[0].name + '\n');
+				console.log(obj.id + '\n')
+			});
+		} else {
+			console.log(data);
+		}
+	});
 }
